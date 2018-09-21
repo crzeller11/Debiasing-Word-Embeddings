8a4d5786792e674b8451923d0704c9e75ce7a7c6
Unknown <czeller@oxy.edu>
2018-09-21 12:08:57 -0700

    .gitignore, minor file changes

--- Data_Processing/process_data.py
+++ Data_Processing/process_data.py
@@ -15,11 +15,16 @@ def make_corpus(infile, outfile):
     for text in wiki.get_texts():
         output.write(bytes(' '.join(text), 'utf-8').decode('utf-8') + '\n')
         i += 1
-        if (i % 100 == 0):
+        if (i % 10000 == 0):
             print('Processed ' + str(i) + ' articles so far.')
     output.close()
     print('Processing complete! Yippee!')
 
+# helper method if you don't want to use command line
+def run_me():
+    infile = 'enwiki-latest-pages-articles21.xml-p21222161p22722161'
+    outfile = 'output_corpus.txt'
+    make_corpus(infile, outfile)
 
 if __name__ == '__main__':
     if len(sys.argv) != 3:
