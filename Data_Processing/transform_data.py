ac3727b8dbc50d8534407493e741d8cb02a0a966
Unknown <czeller@oxy.edu>
2018-10-10 21:28:47 -0700

    Discovered more pesky pronoun issues

--- Data_Processing/transform_data.py
+++ Data_Processing/transform_data.py
@@ -1,11 +1,25 @@
 ORIGINAL_FILEPATH = '/Users/chloerainezeller/Desktop/Occidental/Oxy - Fourth Year/First Semester/COMPSCI COMPS/Debiasing-Word-Embeddings/fastText/data.txt'
 NEW_FILEPATH = 'gender_neutral_data.txt'
 
+# FIXME/TODO
+# Mmmmmm, turns out this is marginally more complicated than I thought...
+# Have to consider a few more edge cases of pronouns, and I'm probably going to have to loop through each word
+# Maybe I can pre-screen and see if the line contains anything of interest, and if so, then loop through individual words...
+change_dict = {
+    'she': 'he',
+    'her': 'him',
+    'hers': 'his',
+    "she's" :"he's",
+    "he's":"she's",
+    'herself':'himself',
+    'himself':'herself'
+}
 
 with open(ORIGINAL_FILEPATH, "rt") as fin:
     with open(NEW_FILEPATH, "wt") as fout:
         for line in fin:
-            line = line.replace(' she ', ' he ').replace(' her ', ' him ').replace(' hers ', ' his ')\
+            # loop through each word, if in dictionary, then replace with it's value
+            line = line.replace(' she ', ' he ').replace(' her', ' him ').replace(' hers ', ' his ')\
                 .replace(' he ', ' she ').replace(' him ', ' her ').replace(' his ', ' hers ')
             fout.write(line)
     fout.close()
@@ -19,7 +33,7 @@ with open(ORIGINAL_FILEPATH) as f:
     f1.close()
 f.close()
 
-
+#
 
 
 
