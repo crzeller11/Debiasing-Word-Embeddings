d7fb03e871e5d845e35030810522144239efe49a
Unknown <czeller@oxy.edu>
2018-10-31 15:11:21 -0700

    Fixed DB statistic norm problems

--- Direct_Bias_Analysis/measure_direct_bias.py
+++ Direct_Bias_Analysis/measure_direct_bias.py
@@ -13,9 +13,12 @@ NEW_MODEL = FastText.load_fasttext_format(NEW_MODEL_FILEPATH)
 
 def get_vecs(model, words):
     vectors = []
+    #print("THIS IS AN UNREAL WORD:", model['ljksdhf']) # important to note that UNREAL WORDS still have vectors that
+    # come out of the model. This is probably why my
     for word in words:
-        vec = model[word]
-        vectors.append(vec)
+        if word in model:
+            vec = model[word]
+            vectors.append(vec)
     return vectors
 
 # get the vectors for all the female words and male words ,then, subtract all male from female (or all female from
@@ -34,14 +37,18 @@ def generate_gender_direction(female_wrds, male_wrds, model):
 
 # calculates direct bias statistic
 def direct_bias(gender_direction, words, model):
-    print("gender direction magnitude:", np.sqrt(gender_direction.dot(gender_direction)))
     algorithm_strictness = 1
     distance_sum = 0
+    count = 0
     for word in words:
-        vector = normalize([model[word]])
-        print('vector magnitude:', np.sqrt(vector.dot(vector)))
-        distance_sum += abs(spatial.distance.cosine(vector, gender_direction)) ** algorithm_strictness # something is very wrong with my vector shape here
-    DB = distance_sum / len(words)
+        if word in model:
+            count += 1
+            vector = model[word] / np.linalg.norm(model[word], ord=1)
+            gender_direction = gender_direction / np.linalg.norm(gender_direction, ord=1)
+            distance_sum += np.dot(vector, gender_direction) ** algorithm_strictness
+            print("Direct Bias for", word, ':', distance_sum)
+        #distance_sum += abs(spatial.distance.cosine(vector, gender_direction)) ** algorithm_strictness # something is very wrong with my vector shape here
+    DB = distance_sum / count
     return DB
 
 
@@ -50,11 +57,11 @@ def main():
     female_words = ['she', 'her', 'hers']
     male_words = ['he', 'him', 'his']
     occupations = ['doctor', 'nurse', 'actor', 'housekeeper', 'mechanic', 'soldier', 'cashier', 'comedian',
-                   'gynecologist', 'musician']
+                   'gynecologist', 'musician', 'waiter', 'waitress', 'king', 'queen']
     old_mdl_DB = direct_bias(generate_gender_direction(male_words, female_words, OLD_MODEL), occupations, OLD_MODEL)
-    new_mdl_DB = direct_bias(generate_gender_direction(male_words, female_words, NEW_MODEL), occupations, NEW_MODEL)
-    print('OLD MODEL DirectBias Statistic on the Basis of 10 gender-neutral occupations:', old_mdl_DB)
-    print('NEW MODEL DirectBias Statistic on the Basis of 10 gender-neutral occupations:', new_mdl_DB)
+    #new_mdl_DB = direct_bias(generate_gender_direction(male_words, female_words, NEW_MODEL), occupations, NEW_MODEL)
+    #print('OLD MODEL DirectBias Statistic on the Basis of 10 gender-neutral occupations:', old_mdl_DB)
+    #print('NEW MODEL DirectBias Statistic on the Basis of 10 gender-neutral occupations:', new_mdl_DB)
 
 
 
