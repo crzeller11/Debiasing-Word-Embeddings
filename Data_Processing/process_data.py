1a8b26a5e1619a5ff3b19648d4c6d3a430ae36b6
Justin Li <justinnhli@gmail.com>
2019-01-04 11:32:48 -0800

    lint

--- Data_Processing/process_data.py
+++ Data_Processing/process_data.py
@@ -1,24 +1,23 @@
-from gensim.corpora import WikiCorpus
-import sys
-
 '''
-Adapted from Matthew Mayo, KDnuggts
+Adapted from Matthew Mayo, KDnuggets
 https://www.kdnuggets.com/2017/11/building-wikipedia-text-corpus-nlp.html
 '''
 
+import sys
+
+from gensim.corpora import WikiCorpus
+
 
 def make_corpus(infile, outfile):
-    output = open(outfile, 'w')
-    wiki = WikiCorpus(infile)
-    i = 0
-    # "text" is actually each individual article
-    for text in wiki.get_texts():
-        output.write(bytes(' '.join(text), 'utf-8').decode('utf-8') + '\n')
-        i += 1
-        if (i % 10000 == 0):
-            print('Processed ' + str(i) + ' articles so far.')
-    output.close()
+    with open(outfile, 'w') as output:
+        wiki = WikiCorpus(infile)
+        # "text" is actually each individual article
+        for i, text in enumerate(wiki.get_texts()):
+            output.write(bytes(' '.join(text), 'utf-8').decode('utf-8') + '\n')
+            if i % 10000 == 0:
+                print('Processed ' + str(i) + ' articles so far.')
     print('Processing complete! Yippee!')
+
 
 # helper method if you don't want to use command line
 def run_me():
@@ -26,10 +25,15 @@ def run_me():
     outfile = 'output_corpus.txt'
     make_corpus(infile, outfile)
 
-if __name__ == '__main__':
+
+def main():
     if len(sys.argv) != 3:
         print('Usage: python3 process_data.py <wikipedia_dump_file> <processed_text_file>')
         sys.exit(1)
     infile = sys.argv[1]
     outfile = sys.argv[2]
     make_corpus(infile, outfile)
+
+
+if __name__ == '__main__':
+    main()
